216eef54bdfc08f0590e03afa9e2f9784e8760f7
Liberty-wwj <98154132+Liberty-wwj@users.noreply.github.com>
2023-02-02 15:54:25 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
@@ -7,23 +7,34 @@ app = Flask(__name__)
 def index():
     return '''
         <form method="post">
-            City in USA: <input type="text" name="city">
-            State in USA: <input type="text" name="state">
+            Zip Code in USA: <input type="text" name="zipcode">
             <input type="submit" value="Submit">
         </form>
     '''
 
-@app.route('/', methods=['POST'])
-def get_zipcode():
-    city = request.form['city']
-    state = request.form['state']
-    result = requests.get(f'https://api.zippopotam.us/us/{state}/{city}').json()
-    # https://api.zippopotam.us/us/ca/milpitas
-    zip_codes = [place['post code'] for place in result['places']]
-    try:
-        return f"Zipcode for {city}: {zip_codes[0]}"
-    except KeyError:
-        return f"No zipcode found for {city}"
+@app.route("/", methods=["POST"])
+def get_weather():
+    zip_code = request.form['zipcode'] 
+    api_key = "83127daeecb54dcc8a2220846230202"
+    weather_url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={zip_code}"
+    weather_data = requests.get(weather_url).json()
+#http://api.weatherapi.com/v1/current.json?key=83127daeecb54dcc8a2220846230202&q=95035
+    if "error" in weather_data:
+        return jsonify({"error": weather_data["error"]}), 400
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    location = weather_data["location"]
+    current = weather_data["current"]
+
+    return jsonify({
+        "location": location["name"],
+        "region": location["region"],
+        "country": location["country"],
+        "temp_c": current["temp_c"],
+        "condition": current["condition"]["text"],
+        "icon": current["condition"]["icon"],
+    })
+    
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+
